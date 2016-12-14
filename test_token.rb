3a81cb6d63c8c9b476a9606ee3413738a355ba66
Batbayar <sukhbaatar_batbayar@unimedia.co.jp>
2016-12-14 17:08:43 +0800

    proc update

--- test_token.rb
+++ test_token.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'awesome_print'
 
 def fb_url
-	ENV['PAGE_ACCESS_TOKEN'] = 'EAAIKvl5UDKkBAAnaCkwzRjjxQZAJEf4kP0Qq7GgWLbtPUJZCx12oZBKkH29h7zcoeFKmJHNZCImM8ASfo3Ve5kvw3Q63ZCMA8OcnIjwZAUFSVpry92w378jXOm7r58yahzkJT6d5MtOCml1rJt29rkZC0mKb70ZARm1C3E2o3gZCe4AZDZD'
   "https://graph.facebook.com/v2.6/me/messages?access_token=" + ENV['PAGE_ACCESS_TOKEN']
 end
 
